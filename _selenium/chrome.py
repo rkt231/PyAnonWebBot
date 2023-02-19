80a6284021a568033931b74ab7339cf611b146c8
rkt231 <rem231@protonmail.com>
2023-02-19 17:38:37 +0100

    WIP: implement undetected chrome

--- _selenium/chrome.py
+++ _selenium/chrome.py
@@ -1,4 +1,7 @@
-from seleniumwire import webdriver as WD
+try:
+    import undetected_chromedriver as UC
+except ImportError:
+    from seleniumwire import webdriver as WD
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
@@ -22,25 +25,37 @@ def init_chrome(proxy=False, headers=False):
     """
     _wire_opts = False
     _serv = ChromeService(executable_path=ChromeDriverManager().install())
-    _opts = WD.ChromeOptions()
+    if UC:
+        _opts = UC.ChromeOptions()
+    else:
+        _opts = WD.ChromeOptions()
     if conf.HEADLESS:
         _opts.add_argument('headless')
     #_opts.add_argument('--disable-infobars')
     #_opts.add_argument('--disable-dev-shm-usage')
     #_opts.add_argument('--no-sandbox')
     if proxy:
-        _wire_opts = {'proxy':conf.PROXIES}
-        #PROXY = conf.PROXIES['https']
-        #_opts.add_argument('--proxy-server=%s' % PROXY)
+        if UC:
+            _opts.add_argument('--proxy-server=%s' % conf.PROXIES['https'])
+        else:
+            _wire_opts = {'proxy':conf.PROXIES}
     if not headers:
         headers = rand_user_agent.rand_uagent()
     _opts.add_argument("user-agent=%s" % headers)
-    if not _wire_opts:
-        driver = WD.Chrome(service=_serv, options=_opts, \
-                           service_args=['--verbose', '--log-path=/tmp/chromedriver.log'])
+    if UC:
+        driver = UC.Chrome(service=_serv, options=_opts, \
+                           service_args=['--verbose', \
+                           '--log-path=/tmp/chromedriver.log'])
     else:
-        driver = WD.Chrome(service=_serv, seleniumwire_options=_wire_opts, options=_opts, \
-                           service_args=['--verbose', '--log-path=/tmp/chromedriver.log'])
+        if not _wire_opts:
+            driver = WD.Chrome(service=_serv, options=_opts, \
+                               service_args=['--verbose', \
+                               '--log-path=/tmp/chromedriver.log'])
+        else:
+            driver = WD.Chrome(service=_serv, seleniumwire_options=_wire_opts, \
+                               options=_opts, \
+                               service_args=['--verbose', \
+                               '--log-path=/tmp/chromedriver.log'])
     #driver.request_interceptor = interceptor
     user_agent = driver.execute_script("return navigator.userAgent;")
     print("User agent: %s" % user_agent)
